98dccc536d5d7ce7f1e9ae6fe065f068684c4421
cigkofteci <cigkofteci@github.com>
2021-02-10 18:27:44 +0300

    EntityFramework altyapsı eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,20 +1,23 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    // Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(product.ProductName);
             }
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
