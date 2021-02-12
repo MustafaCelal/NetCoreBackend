d512a7a64c00ff7152d7d1f7b55b4c3707e07af1
cigkofteci <cigkofteci@github.com>
2021-02-12 09:42:00 +0300

    9.gün sonu

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,22 +1,6 @@
-﻿using Entities.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
-
-namespace DataAccess.Abstract
+﻿namespace DataAccess.Abstract
 {
-    //Generic Constraint
-    // class: referans tip
-    // IEntity: IEntity olabilir veya IEntity implement eden bir nesne olabilir
-    // new: new'lenebilir olmalı
-    public interface IEntityRepository<T> where T : class, IEntity, new()
+    public interface IEntityRepository<T1, T2>
     {
-        List<T> GetAll(Expression<Func<T, bool>> filter = null);
-        T Get(Expression<Func<T, bool>> filter);
-        void Add(T entity);
-        void Update(T entity);
-        void Delete(T entity);
-        //List<T> GetAllByCategory(int categoryId);
     }
 }
---
+++ Entities/DTOs/ProductDetailDto.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class ProductDetailDto:IDto
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string CateogryName { get; set; }
+        public short UnitsInStock { get; set; }
+
+    }
+}
