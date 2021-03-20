bb58b9f46c350a666317000eb5e02123e5a0dfe0
cigkofteci <cigkofteci@github.com>
2021-03-20 03:33:19 +0300

    14. Gün ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,15 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Kategorideki ürün adedini aştınız!";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
         public static string CategegoryLimitExceded = "Kategory Limiti aşıldığı için Yeni ürün eklenemiyor";
+
+        public static string AuthorizationDenied = "Yetkiniz yok";
+
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+
+        public static string PasswordError = "Parola hatası";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated { get; internal set; }
+        public static string UserRegistered = "Kayıt oldu";
     }
 }
--- Core/Entities/Concrete/UserOperatiomClaim.cs
+++ Core/Entities/Concrete/UserOperatiomClaim.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.Entities.Concrete
 {
-    public class UserOperatiomClaim:IEntity
+    public class UserOperationClaim:IEntity
     {
         public int Id { get; set; }
         public int UserId { get; set; }
